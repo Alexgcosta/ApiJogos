f9485ba5818fd3b011db37e49bb1efe7c23949e4
Alexgcosta <alexgcosta81@gmail.com>
2021-10-12 21:26:03 -0300

    - Estrutura inicial

---
+++ ApiJogos/Repositories/Jogo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiJogos.Repositories
+{
+    public class Jogo
+    {
+        public string Produtora { get; internal set; }
+        public string Nome { get; internal set; }
+        public double Preco { get; internal set; }
+        public Guid Id { get; internal set; }
+    }
+}
